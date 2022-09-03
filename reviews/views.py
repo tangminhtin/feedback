6810f7002c923fd072d7564504c5fdca5dfbaad7
Minh Tin Tang <minhtintang@gmail.com>
2022-09-03 22:09:45 +0700

    03-class-view

--- reviews/views.py
+++ reviews/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponseRedirect
 from django.views import View
 from django.views.generic.base import TemplateView
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import FormView, CreateView
 
 from .forms import ReviewForm
 from .models import Review
@@ -11,22 +12,39 @@ from .models import Review
 # Create your views here.
 
 
-class ReviewView(View):
-    def get(self, request):
-        form = ReviewForm()
-        return render(request, "reviews/review.html", {
-            "form": form,
-        })
+# class ReviewView(View):
+#     def get(self, request):
+#         form = ReviewForm()
+#         return render(request, "reviews/review.html", {
+#             "form": form,
+#         })
 
-    def post(self, request):
-        form = ReviewForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect('/thank-you')
+#     def post(self, request):
+#         form = ReviewForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return HttpResponseRedirect('/thank-you')
 
-        return render(request, "reviews/review.html", {
-            "form": form,
-        })
+#         return render(request, "reviews/review.html", {
+#             "form": form,
+#         })
+
+
+# class ReviewView(FormView):
+#     form_class = ReviewForm
+#     template_name = 'reviews/review.html'
+#     success_url = "/thank-you"
+
+#     def form_valid(self, form):
+#         form.save()
+#         return super().form_valid(form)
+
+
+class ReviewView(CreateView):
+    model = Review
+    form_class = ReviewForm
+    template_name = 'reviews/review.html'
+    success_url = '/thank-you'
 
 
 class ThankYouView(TemplateView):
